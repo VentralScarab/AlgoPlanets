1b68d0765f94c3c243155e746f757b346cb4d57d
Ventral <86186530+VentralScarab@users.noreply.github.com>
2021-10-20 09:43:53 +0200

    Update Generate-ARC69-JSON.py

--- Generate-ARC69-JSON.py
+++ Generate-ARC69-JSON.py
@@ -2,6 +2,8 @@ with open("Unicode.txt", "r") as file:
     
     for line in file:
         
+        n=1
+        
         Type = ""
         Color = ""
         Liquid = ""
@@ -126,6 +128,9 @@ with open("Unicode.txt", "r") as file:
             Liquid = "Magma Rivers"
             
         #Craters
+        if line[0] == "6":
+            Crater = "Cracks"
+            
         if line[5] == "0":
             Crater = "No Craters"
       
@@ -152,6 +157,9 @@ with open("Unicode.txt", "r") as file:
             Volcano = "No Volcano"
             
         #Clouds
+        if line[0] == "5":
+            Clouds = "Dwarf Clouds"
+            
         if line[8] == "1":
             Clouds = "Veil Clouds"
 
@@ -184,3 +192,173 @@ with open("Unicode.txt", "r") as file:
 
         elif line[8] == "l":
             Clouds = "No Clouds"
+        
+        #Moons
+        if line [0] == "5":
+            Moons = "Two Moons Purple and Red"
+            
+        if line [9] == "0":
+            Moons = "No Moons"
+
+        elif line[9] == "1":
+            Moons = "One Moon Brown"
+
+        elif line[9] == "2":
+            Moons = "One Moon Green"
+
+        elif line[9] == "3":
+            Moons = "One Moon Iron"
+
+        elif line[9] == "4":
+            Moons = "One Moon Blue"
+
+        elif line[9] == "5":
+            Moons = "One Moon Red"
+
+        elif line[9] == "6":
+            Moons = "Two Moons Brown"
+
+        elif line[9] == "7":
+            Moons = "Two Moons Green"
+
+        elif line[9] == "8":
+            Moons = "Two Moons Iron"
+
+        elif line[9] == "9":
+            Moons = "Two Moons Blue"
+
+        elif line[9] == "k":
+            Moons = "Two Moons Red"
+
+        elif line[9] == "l":
+            Moons = "Two Moons Brown and Green"
+
+        elif line[9] == "m":
+            Moons = "Two Moons Brown and Iron"
+
+        elif line[9] == "n":
+            Moons = "Two Moons Brown and Blue"
+
+        elif line[9] == "o":
+            Moons = "Two Moons Brown and Red"
+
+        elif line[9] == "p":
+            Moons = "Two Moons Green and Iron"
+
+        elif line[9] == "q":
+            Moons = "Two Moons Green and Blue"
+
+        elif line[9] == "r":
+            Moons = "Two Moons Green and Red"
+
+        elif line[9] == "s":
+            Moons = "Two Moons Iron and Blue"
+
+        elif line[9] == "t":
+            Moons = "Two Moons Iron and Red"
+
+        elif line[9] == "u":
+            Moons = "Two Moons Blue and Red"
+
+        elif line[9] == "v":
+            Moons = "No Moons"
+            
+        #Rings
+        if line[10] == "0"
+            Rings = image.open("No Rings.png")
+
+        elif line[10] == "1":
+            Rings = image.open("One Ring Brown.png")
+
+        elif line[10] == "2":
+            Rings = image.open("One Ring Brown (Vertical).png")
+
+        elif line[10] == "3":
+            Rings = image.open("One Ring Iron.png")
+
+        elif line[10] == "4":
+            Rings = image.open("One Ring Iron (Vertical).png")
+
+        elif line[10] == "5":
+            Rings = image.open("One Ring Green.png")
+
+        elif line[10] == "6":
+            Rings = image.open("One Ring Green (Vertical).png")
+
+        elif line[10] == "7":
+            Rings = image.open("Two Rings Brown (One Vertical).png")
+
+        elif line[10] == "8":
+            Rings = image.open("Two Rings Iron (One Vertical).png")
+
+        elif line[10] == "9":
+            Rings = image.open("Two Rings Green (One Vertical).png")
+
+        elif line[10] == "k":
+            Rings = image.open("Two Rings Brown and Green (One Vertical).png")
+
+        elif line[10] == "l":
+            Rings = image.open("Two Rings Grey and Brown (One Vertical).png")
+
+        elif line[10] == "m":
+            Rings = image.open("Two Rings Green and Grey (One Vertical).png")
+
+        elif line[10] == "n":
+            Rings = image.open("No Rings.png")
+
+        elif line[10] == "o":
+            Rings = image.open("Two Rings Brown (45°).png")
+
+        elif line[10] == "p":
+            Rings = image.open("Two Rings Iron (45°).png")
+
+        elif line[10] == "q":
+            Rings = image.open("Two Rings Green (45°).png")
+
+        elif line[10] == "r":
+            Rings = image.open("Two Rings Brown and Green (45°).png")
+
+        elif line[10] == "s":
+            Rings = image.open("Two Rings Grey and Brown (45°).png")
+
+        elif line[10] == "t":
+            Rings = image.open("Two Rings Green and Grey (45°).png")
+
+        elif line[10] == "u":
+            Rings = image.open("No Rings.png")
+            
+        metadata = {   
+                        '"standard"': '"arc69"',
+                        '"description"': '"AlgoPlanet ' + n'"'
+                        '"attributes"': [
+                            {'"trait_type"':'"Type"',
+                            '"value"':'"'Type'"'
+                            },
+                            {'"trait_type"':'"Color"',
+                            '"value"':'"'Color'"'
+                            },
+                            {'"trait_type"':'"Liquid"',
+                            '"value"':'"'Liquid'"'
+                            },
+                            {'"trait_type"':'"Crater"',
+                            '"value"':'"'Crater'"'
+                            },
+                            {'"trait_type"':'"Volcano"',
+                            '"value"':'"'Volcano'"'
+                            },
+                            {'"trait_type"':'"Clouds"',
+                            '"value"':'"'Clouds'"'
+                            },
+                            {'"trait_type"':'"Moons"',
+                            '"value"':'"'Moons'"'
+                            },
+                            {'"trait_type"':'"Rings"',
+                            '"value"':'"'Rings'"'
+                            }
+                        ]
+                    }
+            
+        with open("AlgoPlanetMetadata" + n + ".json", "r+") as f:
+            f.write(metadata)
+            n+=1
+        f.close
