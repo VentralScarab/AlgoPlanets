f903e8999a36c3ee0aa76f985e8b370656bc22fa
Ventral <86186530+VentralScarab@users.noreply.github.com>
2021-10-22 13:02:44 +0200

    Update Randomly-Generated-Planet.py

--- Randomly-Generated-Planet.py
+++ Randomly-Generated-Planet.py
@@ -1,35 +1,35 @@
 import random
 i = 0
 while i < 512:
-    SolidGas = ["Terrestrial World","Gas Giant","Ice Giant", "Lava Planet", "Dwarf Planet", "Algo Planet", "Moon Impact"] 
-    OddsSolidGas = [500,300,200,2,2,2,2] 
+    SolidGas = ["Terrestrial World","Gas Giant","Ice Giant", "Lava Planet", "Dwarf Planet", "Moon Impact"] 
+    OddsSolidGas = [500,300,200,2,2,2] 
     SolidPlanets = ["Blue Terrestrial Planet", "Green Terrestrial Planet", "Brown Terrestrial Planet", "Iron Terrestrial Planet", "Red Terrestrial Planet", "Yellow Terrestrial Planet", "Purple Terrestrial Planet"] #Chance 1/7
     GasPlanets = ["Purple Gas Planet", "Brown Gas Planet", "Blue/Turquoise Gas Planet", "Red Gas Planet"] #Chance 1/4
     IceGiant = ["Dark Blue Planet", "Light Blue Planet", "Icy White Planet"] #Chance 1/3
     Water = ["Oceans", "River","Frozen Oceans", "Frozen Rivers", "Dry Oceans", "Dry Rivers", "Waterless"] 
-    OddsWater = [17,25,9,11.5,2.25,3,32.25] 
+    OddsWater = [70,100,20,25,1.5,2,75] 
     WaterAndMethane = ["Methane Oceans", "Methane River", "Oceans", "River","Frozen Oceans", "Frozen Rivers", "Dry Oceans", "Dry Rivers", "Waterless"]
-    OddsWaterAndMethane = [8.5,11,17,25,9,11.5,2.25,3,12.75] 
+    OddsWaterAndMethane = [10,14,45,65,15,20,1.5,2,60] 
     WaterBluePlanet = ["Dry Oceans", "Dry Rivers", "Waterless"]
-    OddsWaterBluePlanet = [2.25,3,94.75]
+    OddsWaterBluePlanet = [1.5,2,120]
     Clouds = ["Veil Water Clouds (White)", "Stratocumulus Water Clouds (White)","Cumulus Water Clouds (White)", "Cloudless"]
-    OddsClouds = [20,8,28,44] 
+    OddsClouds = [30,15,40,160] 
     GasPlanetClouds = ["Cumulus Water Clouds (White)","Cumulus Silicate Clouds (Red)","Cumulus Ammonia Clouds (Orange)", "Storm Clouds (White)", "Storm Silicate Clouds (Red)", "Storm Ammonia Clouds (Orange)", "Cloudless"]
-    OddsGasPlanetClouds = [28,15,15, 6, 4, 4, 28] 
-    Moons = ["One Moon Brown", "One Moon Brown (Craters)", "One Moon Grey/Iron", "One Moon Grey/Iron (Craters)","One Moon Green", "One Moon Green (Craters)", "Two Moons Brown", "Two Moons Brown (Craters)", "Two Moons Green", "Two Moons Green (Craters)", "Two Moons Grey/Iron", "Two Moons Grey/Iron (Craters)", "Two Moons Brown&Green", "Two Moons Brown&Green (Craters)", "Two Moons Brown&Grey","Two Moons Brown&Grey (Craters)", "Two Moons Green&Grey","Two Moons Green&Grey (Craters)", "Two Moons Brown&Green (Green with Craters)", "Two Moons Brown&Green (Brown with Craters)", "Two Moons Green&Grey (Grey with Craters)", "Two Moons Green&Grey (Green with Craters)", "Two Moons Brown&Grey (Brown with Craters)", "Two Moons Brown&Grey (Grey with Craters)", "No Moons"]
-    OddsMoons = [0.8,0.8,0.8,0.8,0.8,0.8,0.45,0.45,0.45,0.45,0.45,0.45,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,89.2] 
+    OddsGasPlanetClouds = [20,10,10, 2.5, 1, 1, 75] 
+    Moons = ["One Moon Brown", "One Moon Grey/Iron", "One Moon Green", "One Moon Blue", "One Moon Red", "Two Moons Brown", "Two Moons Green", "Two Moons Grey/Iron", "Two Moons Blue", "Two Moons Red", "Two Moons Brown&Green", "Two Moons Brown&Grey", "Two Moons Brown&Blue", "Two Moons Brown&Red", "Two Moons Green&Grey", "Two Moons Green&Blue", "Two Moons Green&Red", "Two Moons Grey&Blue", "Two Moons Grey&Red", "Two Moons Blue&Red", "No Moons"]
+    OddsMoons = [1,1,1,1,1,1,0.5,0.5,0.5,0.5,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,140] 
     Rings = ["One Brown Ring", "One Brown Ring Vertical", "One Black/Grey Ring", "One Black/Grey Ring Vertical", "One Green Ring", "One Green Ring Vertical", "Two Brown Rings One Vertical", "Two Black/Grey Rings One Vertical", "Two Green Rings One Vertical", "Two Brown&Green Rings One Vertical", "Two Grey & Brown Rings One Vertical", "Two Grey&Green Rings One Vertical", "No Rings"]
-    OddsRings = [6,6,6,6,6,6,3,3,3,1.5,1.5,1.5,50.5]
+    OddsRings = [15,15,15,15,15,15,2,2,2,1,1,1,100]
     RingsWithNoMoon = ["Two Brown Rings(45°)", "Two Black/Grey Rings (45°)", "Two Green Rings (45°)", "Two Brown&Green Rings (45°)", "Two Grey&Brown Rings (45°)", "Two Grey&Green Rings (45°)", "No Rings"]
-    OddsRingsWithNoMoon = [1.5,1.5,1.5,0.75,0.75,0.75,93.25] 
+    OddsRingsWithNoMoon = [0.8,0.8,0.8,0.3,0.3,0.3,120] 
     Crater = ["Large Impact Crater", "Large Impact Crater & Volcanic Caldera Craters","Volcanic Caldera Craters", "No Craters"]
-    OddsCrater = [15,8,25,52] 
+    OddsCrater = [7.5,4,20,100] 
     Volcano = ["Volcano", "No Volcano"]
-    OddsVolcano = [17.8,82.2] 
+    OddsVolcano = [2,150] 
     Magma = ["Magma Ocean", "Magma River", "No Magma"]
-    OddsMagma = [13,20.5,66.5]
+    OddsMagma = [3,7,150]
     Planet = []
-    text = '# \n'
+    text = ''
 
     #Terrastrial
     SolidGasDecide = random.choices(SolidGas, OddsSolidGas, k=1)
@@ -271,7 +271,8 @@ while i < 512:
 
     Convert = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     AllClouds = Clouds + GasPlanetClouds
-    Digits = [SolidGas,SolidPlanets,GasPlanets,IceGiant,WaterAndMethane,Crater,Magma,Volcano,AllClouds,Moons,RingsWithNoMoon] 
+    AllRings = Rings + RingsWithNoMoon
+    Digits = [SolidGas,SolidPlanets,GasPlanets,IceGiant,WaterAndMethane,Crater,Magma,Volcano,AllClouds,Moons,AllRings] 
     Unicode = ''
     for Digit in Digits:
         found = False
@@ -285,11 +286,16 @@ while i < 512:
         if found == False:
             code = 0       
         Unicode = Unicode + str(code)
-    test = '20300000416'
-    text = text + '\n' + Unicode + '\n#\n'
+    text =text + Unicode + '\n'
 
     with open('AlgoPlanet.txt', 'r+') as f:
-        if test not in f.read():
+        if Unicode not in f.read():
             f.write(text)
+            i += 1
+
+    text2 = Unicode + '\n'
+
+    with open('AlgoPlanet_Unicode.txt', 'r+') as f2:
+        if Unicode not in f2.read():
+            f2.write(text2)
     f.close
-    i += 1
