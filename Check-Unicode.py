4f245746450dbd739d5e77396f591f7c51835609
Ventral <86186530+VentralScarab@users.noreply.github.com>
2021-10-22 13:06:58 +0200

    Update Check-Unicode.py

--- Check-Unicode.py
+++ Check-Unicode.py
@@ -11,16 +11,19 @@ with open("AlgoPlanet_Unicode.txt","r") as file:
         if line[0] == "4" or line[0] == "5" or line[0] == "6" or line[0] == "7":
             Secret +=1    
 
-        elif line[4] == "7" or line[4] == "8" or line[7] == "1" or line[5] == "2" or line[6] == "1" or line[9] == "n" or line[9] == "o" or line[9] == "p" or line[9] == "q" or line[9] == "r" or line[9] == "s" or line[9] == "t" or line[9] == "u" or line[9] == "v" or line[9] == "w" or line[9] == "x" or line[9] == "y" or line[10] == "o" or line[10] == "p" or line[10] == "q" or line[10] == "r" or line[10] == "s" or line[10] == "t":
+        elif line[4] == "7" or line[4] == "8" or line[7] == "1" or line[5] == "2" or line[6] == "1" or line[9] == "l" or line[9] == "m" or line[9] == "n" or line[9] == "o" or line[9] == "p" or line[9] == "q" or line[9] == "r" or line[9] == "s" or line[9] == "t" or line[9] == "u" or line[10] == "o" or line[10] == "p" or line[10] == "q" or line[10] == "r" or line[10] == "s" or line[10] == "t":
             UR +=1
 
-        elif line[4] == "1" or line[4] == "2" or line[5] == "1" or line[6] == "2" or line[9] == "7" or line[9] == "8" or line[9] == "9" or line[9] == "k" or line[9] == "l" or line[9] == "m" or line[10] == "7" or line[10] == "8" or line[10] == "9" or line[10] == "k" or line[10] == "l" or line[10] == "m": 
+        elif line[4] == "1" or line[4] == "2" or line[5] == "1" or line[6] == "2" or line[9] == "6" or line[9] == "7" or line[9] == "8" or line[9] == "9" or line[9] == "k" or line[10] == "7" or line[10] == "8" or line[10] == "9" or line[10] == "k" or line[10] == "l" or line[10] == "m": 
             SR +=1
 
-        elif line[4] == "5" or line[4] == "6" or line[5] == "3" or line[8] == "2" or line[8] == "8" or line[8] == "9" or line[8] == "k" or line[9] == "1" or line[9] == "2" or line[9] == "3" or line[9] == "4" or line[9] == "5" or line[9] == "6":
+        elif line[4] == "5" or line[4] == "6" or line[5] == "3" or line[8] == "2" or line[8] == "8" or line[8] == "9" or line[8] == "k" or line[9] == "1" or line[9] == "2" or line[9] == "3" or line[9] == "4" or line[9] == "5":
             R +=1
 
-        else:
+        #elif line[4] == "3" or line[4] == "4" or line[4] == "9" or line[8] == "1" or line[8] == "3" or line[8] == "4" or line[8] == "5" or line[8] == "6" or line[8] == "7" or line[8] == "l" or line[9] == "z" or line[5] == "4" or line[6] == "3" or line[7] == "2" or line[10] == 'n' or line[10] == "u": 
+            #C +=1
+
+        else: 
             C +=1
          
 print("Secret Rare =", Secret)  
